9033141fca76bd934bb408f4694d84d125b8fbab
Juan Batiz-Benet <juan@benet.ai>
2014-07-04 11:21:39 -0700

    gofmt

--- index.go
+++ index.go
@@ -1,69 +1,69 @@
 package multiaddr
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 type Multiaddr struct {
-  Bytes []byte
+	Bytes []byte
 }
 
 func NewMultiaddr(s string) (*Multiaddr, error) {
-  b, err := StringToBytes(s)
-  if err != nil {
-    return nil, err
-  }
-  return &Multiaddr{Bytes: b}, nil
+	b, err := StringToBytes(s)
+	if err != nil {
+		return nil, err
+	}
+	return &Multiaddr{Bytes: b}, nil
 }
 
 func (m *Multiaddr) String() (string, error) {
-  return BytesToString(m.Bytes)
+	return BytesToString(m.Bytes)
 }
 
 func (m *Multiaddr) Protocols() (ret []*Protocol, err error) {
 
-  // panic handler, in case we try accessing bytes incorrectly.
-  defer func() {
-    if e := recover(); e != nil {
-      ret = nil
-      err = e.(error)
-    }
-  }()
+	// panic handler, in case we try accessing bytes incorrectly.
+	defer func() {
+		if e := recover(); e != nil {
+			ret = nil
+			err = e.(error)
+		}
+	}()
 
-  ps := []*Protocol{}
-  b := m.Bytes[:]
-  for ; len(b) > 0 ; {
-    p := ProtocolWithCode(int(b[0]))
-    if p == nil {
-      return nil, fmt.Errorf("no protocol with code %d", b[0])
-    }
-    ps = append(ps, p)
-    b = b[1 + (p.Size / 8):]
-  }
-  return ps, nil
+	ps := []*Protocol{}
+	b := m.Bytes[:]
+	for len(b) > 0 {
+		p := ProtocolWithCode(int(b[0]))
+		if p == nil {
+			return nil, fmt.Errorf("no protocol with code %d", b[0])
+		}
+		ps = append(ps, p)
+		b = b[1+(p.Size/8):]
+	}
+	return ps, nil
 }
 
 func (m *Multiaddr) Encapsulate(o *Multiaddr) *Multiaddr {
-  b := make([]byte, len(m.Bytes) + len(o.Bytes))
-  b = append(m.Bytes, o.Bytes...)
-  return &Multiaddr{Bytes: b}
+	b := make([]byte, len(m.Bytes)+len(o.Bytes))
+	b = append(m.Bytes, o.Bytes...)
+	return &Multiaddr{Bytes: b}
 }
 
 func (m *Multiaddr) Decapsulate(o *Multiaddr) (*Multiaddr, error) {
-  s1, err := m.String()
-  if err != nil {
-    return nil, err
-  }
+	s1, err := m.String()
+	if err != nil {
+		return nil, err
+	}
 
-  s2, err := o.String()
-  if err != nil {
-    return nil, err
-  }
+	s2, err := o.String()
+	if err != nil {
+		return nil, err
+	}
 
-  i := strings.LastIndex(s1, s2)
-  if i < 0 {
-    return nil, fmt.Errorf("%s not contained in %s", s2, s1)
-  }
-  return NewMultiaddr(s1[:i])
+	i := strings.LastIndex(s1, s2)
+	if i < 0 {
+		return nil, fmt.Errorf("%s not contained in %s", s2, s1)
+	}
+	return NewMultiaddr(s1[:i])
 }
--- protocols.go
+++ protocols.go
@@ -1,9 +1,9 @@
 package multiaddr
 
 type Protocol struct {
-  Code int
-  Size int
-  Name string
+	Code int
+	Size int
+	Name string
 }
 
 // replicating table here to:
@@ -12,31 +12,31 @@ type Protocol struct {
 // 3. changing a number has to happen in two places.
 
 var Protocols = []*Protocol{
-  &Protocol{4,  32,  "ip4"},
-  &Protocol{6,  16,  "tcp"},
-  &Protocol{17, 16,  "udp"},
-  &Protocol{33, 16,  "dccp"},
-  &Protocol{41, 128, "ip6"},
-  // these require varint:
-  &Protocol{132, 16, "sctp"},
-  // {480, 0, "http"},
-  // {443, 0, "https"},
+	&Protocol{4, 32, "ip4"},
+	&Protocol{6, 16, "tcp"},
+	&Protocol{17, 16, "udp"},
+	&Protocol{33, 16, "dccp"},
+	&Protocol{41, 128, "ip6"},
+	// these require varint:
+	&Protocol{132, 16, "sctp"},
+	// {480, 0, "http"},
+	// {443, 0, "https"},
 }
 
 func ProtocolWithName(s string) *Protocol {
-  for _, p := range(Protocols) {
-    if p.Name == s {
-      return p
-    }
-  }
-  return nil
+	for _, p := range Protocols {
+		if p.Name == s {
+			return p
+		}
+	}
+	return nil
 }
 
 func ProtocolWithCode(c int) *Protocol {
-  for _, p := range(Protocols) {
-    if p.Code == c {
-      return p
-    }
-  }
-  return nil
+	for _, p := range Protocols {
+		if p.Code == c {
+			return p
+		}
+	}
+	return nil
 }
