47a6842e3507c1dc3214375815ec0c6f6467be30
Juan Batiz-Benet <juan@benet.ai>
2014-07-03 23:44:09 -0700

    rm convert

--- codec.go
+++ codec.go
@@ -3,6 +3,9 @@ package multiaddr
 import(
   "fmt"
   "strings"
+  "encoding/binary"
+  "net"
+  "strconv"
 )
 
 
@@ -48,3 +51,40 @@ func BytesToString(b []byte) (string, error) {
 
   return s, nil
 }
+
+func AddressStringToBytes(p *Protocol, s string) []byte {
+  switch p.Code {
+
+  // ipv4,6
+  case 4, 41:
+    return net.ParseIP(s).To4()
+
+  // tcp udp dccp sctp
+  case 6, 17, 33, 132:
+    b := make([]byte, 2)
+    i, err := strconv.Atoi(s)
+    if err == nil {
+      binary.BigEndian.PutUint16(b, uint16(i))
+    }
+    return b
+  }
+
+  return []byte{}
+}
+
+func AddressBytesToString(p *Protocol, b []byte) string {
+  switch p.Code {
+
+  // ipv4,6
+  case 4, 41:
+    return net.IP(b).String()
+
+  // tcp udp dccp sctp
+  case 6, 17, 33, 132:
+    i := binary.BigEndian.Uint16(b)
+    return strconv.Itoa(int(i))
+  }
+
+  return ""
+}
+
