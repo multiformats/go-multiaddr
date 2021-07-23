5e9a3ae8cb66cd5c036dde777f0141e56c470f67
Marten Seemann <martenseemann@gmail.com>
2021-07-23 11:31:01 +0200

    remove deprecated NetCodec

--- net/registry_test.go
+++ net/registry_test.go
@@ -7,44 +7,36 @@ import (
 	ma "github.com/multiformats/go-multiaddr"
 )
 
-func TestRegisterSpec(t *testing.T) {
+func TestRegisterFrom(t *testing.T) {
 	cm := NewCodecMap()
-	myproto := &NetCodec{
-		ProtocolName:     "test",
-		NetAddrNetworks:  []string{"test", "iptest", "blahtest"},
-		ConvertMultiaddr: func(a ma.Multiaddr) (net.Addr, error) { return nil, nil },
-		ParseNetAddr:     func(a net.Addr) (ma.Multiaddr, error) { return nil, nil },
-	}
-
-	cm.RegisterNetCodec(myproto)
-
-	_, ok := cm.addrParsers["test"]
-	if !ok {
+	cm.RegisterFromNetAddr(
+		func(a net.Addr) (ma.Multiaddr, error) { return nil, nil },
+		"test", "iptest", "blahtest",
+	)
+	if _, ok := cm.addrParsers["test"]; !ok {
 		t.Fatal("myproto not properly registered")
 	}
-
-	_, ok = cm.addrParsers["iptest"]
-	if !ok {
+	if _, ok := cm.addrParsers["iptest"]; !ok {
 		t.Fatal("myproto not properly registered")
 	}
-
-	_, ok = cm.addrParsers["blahtest"]
-	if !ok {
+	if _, ok := cm.addrParsers["blahtest"]; !ok {
 		t.Fatal("myproto not properly registered")
 	}
+}
 
-	_, ok = cm.maddrParsers["test"]
-	if !ok {
+func TestRegisterTo(t *testing.T) {
+	cm := NewCodecMap()
+	cm.RegisterToNetAddr(
+		func(a ma.Multiaddr) (net.Addr, error) { return nil, nil },
+		"test", "iptest", "blahtest",
+	)
+	if _, ok := cm.maddrParsers["test"]; !ok {
 		t.Fatal("myproto not properly registered")
 	}
-
-	_, ok = cm.maddrParsers["iptest"]
-	if ok {
+	if _, ok := cm.maddrParsers["iptest"]; !ok {
 		t.Fatal("myproto not properly registered")
 	}
-
-	_, ok = cm.maddrParsers["blahtest"]
-	if ok {
+	if _, ok := cm.maddrParsers["blahtest"]; !ok {
 		t.Fatal("myproto not properly registered")
 	}
 }
