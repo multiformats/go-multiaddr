0d17db1dafe809ba1f7779ce94828247eabd49a9
Chinmay Kousik <chinmaykousik1@gmail.com>
2022-07-12 22:56:02 +0530

    update code to 0x118

--- protocols.go
+++ protocols.go
@@ -36,7 +36,7 @@ const (
 	P_WS                = 0x01DD
 	P_WSS               = 0x01DE // deprecated alias for /tls/ws
 	P_PLAINTEXTV2       = 0x706c61
-	P_WEBRTC            = 0x115
+	P_WEBRTC            = 0x118
 )
 
 var (
